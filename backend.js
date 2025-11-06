17d7b1395faadd6e9a1cc9043eb1ba8776773592
ClaytonGeronimo <geronimiclayton@gmail.com>
2025-11-05 20:08:56 -0500

    2nd update
    - host can start game
    - game picks and shows the player whos turn it is
    - made the random hot zone for the player

--- backend.js
+++ backend.js
@@ -4,25 +4,128 @@
         console.log('Connected to server');
       };
 
+      // waits for users to click the ready button
       document.getElementById("ready").addEventListener('click', newUser);
 
-      function newUser(){
-        const data = {type: 'login', username: document.getElementById("username").value}
-        socket.send(JSON.stringify(data));
-      }
-      socket.onmessage = event => {
-          console.log('Message from server:', event.data);
-        };  
 
+      // depending on the message of the server this determines the action to take
       socket.addEventListener('message', event => {
         const data = JSON.parse(event.data)
-
-        console.log('message from server:', data.message)
-        console.log('adevenlisten')
-        // const targetDiv = document.getElementById('playerNames');
-        // const newh1 = document.createElement('h1');
-        // newh1.textContent = 'player ${ser.message}'
-        // targetDiv.appendChild(newh1)
+        if(data.type == 'updatePlayerBoard'){
+          displayPlayers(data.message)
+        }
+        if(data.type == 'updateHost'){
+          removeHostLogin()
+          AddStartButton()
+        }
+        if(data.type == 'WaitingScreen'){
+          waitingScreen()
+        }
+        if(data.type == 'SelectPlayer'){
+          WhosTurnIsITScreen(data.message)
+          console.log(data)
+        }
       })
+
+      // gets the username inputed by the user and sends it to the server to add to the list of other players
+      function newUser(){
+        const user = document.getElementById("username").value
+        const data = {type: 'login', username: user}
+        socket.send(JSON.stringify(data));
+      }
+
+      // updates the player list everytime a new player joins (only on the host screen)
+      function displayPlayers(data){
+        const targetDiv = document.getElementById('playerNames');
+        clearplayers()
+        data.forEach(player => {
+          const newh1 = document.createElement('h1');
+          newh1.textContent = `${player.username}: `;
+          targetDiv.appendChild(newh1)
+        })  
+      }
+
+
+      //displays slider onto the host screen fro everyone to see 
+      function addSlider(){
+        let randValue = Math.floor(Math.random() * 87 )
+        const targetDiv = document.getElementById('playerNames');
+        const newInput = document.createElement('input');
+        newInput.type = "range"
+        newInput.min = "1"
+        newInput.max = "100"
+        newInput.value =  randValue.toString()
+        newInput.id = "myRange" 
+        newInput.classList.add("slider")
+        targetDiv.appendChild(newInput)
+        sliderColor(randValue)
+      }
+
+      //adds effects to the slider and its colors 
+      function sliderColor(start){
+        const targetTag = document.getElementById('myRange')
+        targetTag.style.background = `linear-gradient(
+        to right,
+        #EDE3C5 0%,
+        #EDE3C5 ${start-4}%,
+        yellow ${start-4}%,
+        yellow ${start-3}%,
+        rgb(245, 65, 10) ${start-3}%,
+        rgb(245, 65, 10) ${start-2}%,
+        lightblue ${start-2}%,
+        lightblue ${start+1}%,
+        rgb(245, 65, 10) ${start+1}%,
+        rgb(245, 65, 10) ${start+2}%,
+        yellow ${start+2}%,
+        yellow ${start+3}%,
+        #EDE3C5 ${start+3}%,
+        #EDE3C5 100%
+  )`
+        
+      }
+
+      // just a function to clear the players before showing the new set of players
+      function clearplayers(){
+        const targetDiv = document.getElementById('playerNames');
+        targetDiv.innerHTML = ''
+      }
+
+      // removes register buttons
+      function removeHostLogin(){
+        const targetDiv = document.getElementById('login');
+        targetDiv.innerHTML = ''
+
+      }
+
+      //adds start button for the host 
+      function AddStartButton(){
+        const targetDiv = document.getElementById('body');
+        const startButton = document.createElement('button')
+        startButton.type = "submit"
+        startButton.id = "startButton"
+        startButton.innerHTML = "START GAME"
+        targetDiv.appendChild(startButton)
+        document.getElementById("startButton").addEventListener('click', startGame);
+
+      }
+
+      // starts the game
+      function startGame(){
+        const data = {type: "startGame"}
+        socket.send(JSON.stringify(data))
+      }
+
+      function WhosTurnIsITScreen(data){
+        const targetDiv = document.getElementById('body');
+        const playerTrun = document.createElement('h1')
+        playerTrun.innerHTML = `${data} turn `
+        targetDiv.appendChild(playerTrun)
+
+      }
+      // updates the secreen to waiting screen when users enter the game
+      function waitingScreen(){
+        removeHostLogin()
+        addSlider()
+      }
 
       
--- server.js
+++ server.js
@@ -1,22 +1,59 @@
 // server.js
 const WebSocket = require('ws');
 const server = new WebSocket.Server({ port: 8080 });
-let players = []
+const clients = new Map()
+const players = []
+playersTurn = 1
 
-server.on('connection', socket => {
-  console.log('🟢 New client connected');
+server.on('connection', ws => {
+  console.log('🟢 New client connected'); 
 
-  socket.on('message', message => {
+  // determines the action to take given a message send from client side
+  ws.on('message', message => {
     const data = JSON.parse(message);
     if (data.type == 'login'){
-      socket.send(JSON.stringify({message: players}))
-      players.push({username: data.username})
-      console.log(players)
+      // adds new players into the db and sends message back to client confirming
+      players.push({username: data.username, points: 0})
+      clients.set(data.username,ws)
+      ws.send(JSON.stringify({message: players}))
+      ws.send(JSON.stringify({type: 'WaitingScreen'}))
+      
+      // update host screen
+      updateHost('host')
+      if(data.username == 'host'){
+        ws.send(JSON.stringify({type: 'updateHost'}))
+      }
+
+
+    }
+    if(data.type == 'startGame'){
+            WhosTurnisIT()
     }
   });
 
+  function WhosTurnisIT(){
+    server.clients.forEach((client) => {
+        if(client.readyState == WebSocket.OPEN && client !== clients.get("host")){
+          const data = {type: 'SelectPlayer' , message: players[playersTurn].username}
+          client.send(JSON.stringify(data))
+        }
+      });
+      playersTurn += 1
+        
+    
+  }
+
+  function updateHost(clientId){
+    const ws = clients.get(clientId)
+    if(ws && ws.readyState == WebSocket.OPEN){
+      ws.send(JSON.stringify({type:'updatePlayerBoard', message: players}))
+    }
+    else{
+      console.log(`client ${clientId} not connected`)
+    }
+  }
   
-  socket.on('close', () => {
+  ws.on('close', () => {
     console.log('🔴 Client disconnected');
   });
 });
